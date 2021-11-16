5f008d3bd77936248120b3efd90f95ba1bfab394
Giruta <irina.giruta@gmail.com>
2021-11-16 08:36:46 +0200

    add section with slider

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(function () {
+
+  // Большой слайдер
+
+  $('.big-slider').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-arrow-alt-circle-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-arrow-alt-circle-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          arrows: false
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+});
