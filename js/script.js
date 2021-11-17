ca072730313398e1289d09bc79273f5178052ed9
Giruta <irina.giruta@gmail.com>
2021-11-17 03:04:12 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $(function () {
 
+  // Menu opener hamburger
+  $('.menu-open, .menu a').click(function () {
+    $('.menu-collapse').toggleClass('d-none').css('order', '1');
+    $('.menu').toggleClass('menu-opened');
+  });
+
   // Большой слайдер
 
   $('.big-slider').slick({
